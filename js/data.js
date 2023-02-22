a3f4ec88a04a7227aec8048f91a8e4c9204c9fd7
nykk520 <nykk520@outlook.com>
2023-02-22 22:53:21 +0800

    init

---
+++ js/data.js
@@ -0,0 +1,168 @@
+// 首页首屏4组下载
+var indexList = [
+    // 第一组
+    {
+        "title": "热门推荐1",
+        "data1": 
+            [   
+                {
+                    'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                    'title': '开源棋牌1',
+                    'android': "http://www.baidu.com1",
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                    'title': '开源棋牌1',
+                    'android': "http://www.baidu.com1",
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                    'title': '开源棋牌2',
+                    'android': "http://www.baidu.com1",
+                    'ios':'http://www.ios.com'
+                },
+                
+            ],
+        //中间的广告
+        "zhong":
+            [
+                {
+                    "img":'https://image.smxjysm.com/cover/01a28000ad28de1acf07bb4d830a5ae5.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    "img":'https://image.smxjysm.com/cover/311cfa455d979cb487db98364d2ad081.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    "img":'https://image.smxjysm.com/cover/a7949e3bfe0d90df9ba1700365ac42c9.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+            ],
+        "data2":
+            [
+                {
+                    'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                    'title': '开源棋牌1',
+                    'android': "http://www.baidu.com1",
+                    'ios':'http://www.ios.com'
+                },
+                
+            ],
+        //下部广告
+        "xia":
+            [
+                {
+                    "img":'https://image.smxjysm.com/cover/01a28000ad28de1acf07bb4d830a5ae5.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    "img":'https://image.smxjysm.com/cover/311cfa455d979cb487db98364d2ad081.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+                {
+                    "img":'https://image.smxjysm.com/cover/a7949e3bfe0d90df9ba1700365ac42c9.jpg',
+                    'android':'http://www.baidu.com',
+                    'ios':'http://www.ios.com'
+                },
+            ],
+    },
+
+
+    // 第2组
+
+    {
+        "title": "热门推荐2",
+        "data1": [
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌2',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            
+        ]
+    },
+
+
+    // 第3组
+
+
+    {
+        "title": "热门推荐3",
+        "data1": [
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌3',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            
+        ]
+    },
+    // 第4组
+
+    {
+        "title": "热门推荐4",
+        "data1": [
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌4',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            {
+                'img': 'https://image.smxjysm.com/cover/13170ed0153d4bc4d2c8bd6016634aaf.jpg',
+                'title': '开源棋牌',
+                'android': "http://www.baidu.com1",
+                'ios':'http://www.ios.com'
+            },
+            
+        ]
+    }
+]
+//底部悬浮广告
+var xuanfu = [
+
+    {
+        "img":'https://image.smxjysm.com/cover/01a28000ad28de1acf07bb4d830a5ae5.jpg',
+        'android':'http://www.baidu.com',
+        'ios':'http://www.ios.com'
+    },
+    {
+        "img":'https://image.smxjysm.com/cover/311cfa455d979cb487db98364d2ad081.jpg',
+        'android':'http://www.baidu.com',
+        'ios':'http://www.ios.com'
+    },
+    {
+        "img":'https://image.smxjysm.com/cover/a7949e3bfe0d90df9ba1700365ac42c9.jpg',
+        'android':'http://www.baidu.com',
+        'ios':'http://www.ios.com'
+    },
+
+]
---
+++ js/main.js
@@ -0,0 +1,141 @@
+
+window.onload = function () {
+    var listTitle = document.getElementById("list-title");
+    var listItems = document.getElementById("list-items");
+    
+    if (indexList.length > 0) {
+        indexList.forEach((item, index) => {
+            var listTitleItem = '';
+            var listDownItem = '';
+            if (index == 0) {
+                setData(item);
+                listTitleItem = '<div class="list-title-item list-title-item-active">' + item.title + '</div>';
+            } else {
+                listTitleItem = '<div class="list-title-item">' + item.title + '</div>';
+            }
+            
+            $(".list-items").append(listDownItem);
+            $(".list-title").append(listTitleItem);
+
+            
+        });
+        
+    }
+
+    // 底部悬浮
+    if(xuanfu.length > 0){
+        var xuanfuHtml = '';
+        xuanfu.forEach((i,x)=>{
+            xuanfuHtml += '<div class="swiper-slide" onclick="goLink(\''+i.android+'\',\''+i.ios+'\')"><img src="'+i.img+'" class="float-img"></div>'
+        })
+        $("#swiper3-wrapper").append(xuanfuHtml);
+        var mySwiper3 = new Swiper('#swiper3', {
+            autoplay: {
+                delay: 2000,
+                stopOnLastSlide: false,
+                disableOnInteraction: false,
+            },
+            loop: true,
+        })
+    }
+
+    $(".list-title-item").click(function () {
+
+        var listTitle = $(".list-title-item");
+        var index = 0;
+        for (var i = 0; i < listTitle.length; i++) {
+            if ($(this).text() == listTitle[i].innerText) {
+                $(this).addClass("list-title-item-active");
+                $(".list-title-slider").attr("style", "width:calc(25%);left:" + i * 25 + "%");
+                index = i;
+            } else {
+                listTitle[i].classList.remove("list-title-item-active");
+            }
+        }
+        var item = indexList[index];
+        setData(item);
+        
+       
+    });
+    
+
+
+   
+}
+
+function setData(item){
+    $(".list-items").empty();
+       
+   var  listDownItem = '<div class="list-tabs list-tabs-active"><div class="list-container">';
+    //第一部分
+    if(item.data1 && item.data1.length > 0){
+        item.data1.forEach((i, x) => {
+            listDownItem += '<div class="list-item"><div class="item-logo"><img class="item-img" src="' + i.img + '" alt=""></div>'
+            listDownItem += ' <p class="item-name">' + i.title + '</p><div class="item-down"><img class="item-down-img" src="./images/icon_download.png" alt=""><p class="item-down-text" onclick="goLink(\''+i.android+'\',\''+i.ios+'\')">下载</p></div></div>'
+        });
+    }
+    listDownItem += '</div>';
+    // 中间横幅
+    if(item.zhong && item.zhong.length > 0){
+        listDownItem += '<div class="list-swiper swiper-container" id="swiper1"><div class="swiper-wrapper">'
+        item.zhong.forEach((i,x)=>{
+            listDownItem += '<div class="swiper-slide" onclick="goLink(\''+i.android+'\',\''+i.ios+'\')"><img class="header-img c1" src="'+i.img+'" alt=""></div>';
+        })
+        listDownItem += '</div></div>';
+    }
+    //第二部分
+    if(item.data2 && item.data2.length > 0){
+        listDownItem += '<div class="list-container" id="3fdd2">';
+        item.data2.forEach((i, x) => {
+            listDownItem += '<div class="list-item"><div class="item-logo"><img class="item-img" src="' + i.img + '" alt=""></div>'
+            listDownItem += ' <p class="item-name">' + i.title + '</p><div class="item-down"><img class="item-down-img" src="./images/icon_download.png" alt=""><p class="item-down-text" onclick="goLink(\''+i.android+'\',\''+i.ios+'\')">下载</p></div></div>'
+        });
+        listDownItem += '</div>';
+    }
+    // 下横幅
+    if(item.xia && item.xia.length > 0){
+        listDownItem += '<div class="list-swiper swiper-container" id="swiper2"><div class="swiper-wrapper">'
+        item.zhong.forEach((i,x)=>{
+            listDownItem += '<div class="swiper-slide" onclick="goLink(\''+i.android+'\',\''+i.ios+'\')"><img class="header-img c1" src="'+i.img+'" alt=""></div>';
+        })
+        listDownItem += '</div></div>';
+    }
+
+    listDownItem += '</div>';
+    $(".list-items").append(listDownItem);
+    //加载swiper
+    if(item.zhong && item.zhong.length > 0){
+        var mySwiper1 = new Swiper('#swiper1', {
+            autoplay: {
+                delay: 2000,
+                stopOnLastSlide: false,
+                disableOnInteraction: false,
+            },
+            loop: true,
+            width: window.innerWidth,
+        })
+    }
+
+    if(item.xia && item.xia.length > 0){
+        var mySwiper2 = new Swiper('#swiper2', {
+            autoplay: {
+                delay: 2000,
+                stopOnLastSlide: false,
+                disableOnInteraction: false,
+            },
+            loop: true,
+        })
+    }
+
+    
+}
+function goLink(a,i) {
+    let u = navigator.userAgent;
+    let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    if (isIOS) {
+        window.open(i, "_blank");
+    } else {
+        window.open(a, "_blank");
+    }
+};
+
